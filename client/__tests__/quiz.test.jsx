987ef78ff9986e1f9e7b755f0966095efd6dbc7c
saasins <sandrasins@gmail.com>
2022-03-04 23:50:43 +0100

    Major messup to try to fix "Unexpected token export"-issue - babel.config. js + .parcelrc

--- client/__tests__/quiz.test.jsx
+++ client/__tests__/quiz.test.jsx
@@ -1,20 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { QuestionContext, Quiz, ShowQuestion } from "../quizApp";
+import { MemoryRouter } from "react-router-dom";
+import { Simulate } from "react-dom/test-utils";
 
-import { ShowAnswer, ShowQuestion } from "../quizApp";
+const question = {
+  question: "Is Jest fun?",
+  answers: {
+    answer_a: "Yes",
+    answer_b: "No",
+    answer_c: "Hell no",
+  },
+  correct_answers: {
+    answer_a_correct: "false",
+    answer_b_correct: "false",
+    answer_c_correct: "true",
+  },
+};
 
 describe("Quiz", () => {
   it("shows random question", () => {
     const element = document.createElement("div");
-    ReactDOM.render(<ShowQuestion />, element);
-    expect(element.querySelector("h1").innerHTML).toEqual("Your question");
+    ReactDOM.render(
+      <MemoryRouter initialEntries={["/question"]}>
+        <QuestionContext.Provider value={{ randomQuestion: () => question }}>
+          <Quiz />
+        </QuestionContext.Provider>
+      </MemoryRouter>,
+      element
+    );
     expect(element.innerHTML).toMatchSnapshot();
   });
 
   it("shows answer", () => {
     const element = document.createElement("div");
-    ReactDOM.render(<ShowAnswer />, element);
-    expect(element.querySelector("h1").innerHTML).toEqual("Your answer is:");
+    ReactDOM.render(
+      <MemoryRouter initialEntries={["/question"]}>
+        <QuestionContext.Provider value={{ randomQuestion: () => question }}>
+          <ShowQuestion />
+        </QuestionContext.Provider>
+      </MemoryRouter>,
+      element
+    );
+    Simulate.click(element.querySelector("[data-testid=answer_a] button"));
     expect(element.innerHTML).toMatchSnapshot();
   });
 });
