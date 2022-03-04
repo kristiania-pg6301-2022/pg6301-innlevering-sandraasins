b05920b4a0d025476e19d193b99299c85b484361
saasins <sandrasins@gmail.com>
2022-03-04 17:35:00 +0100

    Import questions.js and rebuild server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,39 @@
 import express from "express";
+import * as path from "path";
+import {
+  isCorrectAnswer,
+  Questions,
+  randomQuestion,
+} from "../public/questions";
 
 const app = express();
 
-app.get("/api/login", (req, res) => {
-  res.json({
-    username: "admin",
-    fullName: "Noen André Persson",
-  });
+//GET - Returns question with: id, category, questions, answers
+app.get("/api/question", (req, res) => {
+  const { id, category, question, answer } = randomQuestion();
+  res.json({ id, category, question, answers });
+});
+
+//POST - Takes in: id, answer. Returns: "true" || "false"
+app.post("/api/question", (req, res) => {
+  const { id, answer } = req.body;
+  const question = Questions.find((q) => q.id === id);
+  if (isCorrectAnswer({ question, answer })) {
+    return res.json({ result: "true" });
+  } else {
+    return res.json({ return: "false" });
+  }
+});
+
+app.use(express.static("../client/dist"));
+
+//Middleware
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
